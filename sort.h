79243485a17f1b29834935df5ea05cc74c2a5cd5
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-06-26 12:55:22 -0700

    sort

--- sort.h
+++ sort.h
@@ -32,6 +32,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void swap_ints(int *a, int *b);
 
 #endif /* SORT_H */
