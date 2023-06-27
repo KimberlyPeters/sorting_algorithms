1b16e0d00f057572acefa16e76f0c5146ebb00db
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-06-26 19:24:57 -0700

    sort_h update

--- sort.h
+++ sort.h
@@ -33,5 +33,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif /* SORT_H */
